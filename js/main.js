14784b75e96085d17ba21faa7d5a3d02b2e76e62
Гончаров Сергей <Goncharov-5769@main-fer.fnpz.uz>
2022-08-02 17:34:29 +0500

    Доработки

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(function(){
+    $(window).resize(function(){
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+            $('.burger-body .header__button').appendTo($('.header__in'));
+        }
+        if (window.matchMedia("(min-width: 769px)").matches) {
+            $('.burger-body .header__contacts').appendTo($('.header__in'));
+        }
+        if (window.matchMedia("(max-width: 1023px)").matches) {
+            $('.header__button').appendTo($('.burger-body'));
+        }
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            $('.header__contacts').appendTo($('.burger-body'));
+        }
+    });
+    $(window).trigger('resize');
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+        $('.burger-body .header__button').appendTo($('.header__in'));
+    }
+    if (window.matchMedia("(min-width: 769px)").matches) {
+        $('.burger-body .header__contacts').appendTo($('.header__in'));
+    }
+    if (window.matchMedia("(max-width: 1023px)").matches) {
+        $('.header__button').appendTo($('.burger-body'));
+    }
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        $('.header__contacts').appendTo($('.burger-body'));
+    }
+    function burg(){
+        var burgerWr = $('.burger-wrap'),
+            burgerBtn = $('.burger-btn'),
+            burgerBody = $('.burger-body'),
+            burgerCloseBtn = $('.burger-close-btn');
+
+        burgerBtn.on('click', function(){
+            $(burgerWr).addClass('opened');
+            $('html').addClass('ovf');
+        });
+
+        burgerCloseBtn.on('click', function(){
+            $(burgerWr).removeClass('opened');
+            $('html').removeClass('ovf');
+        })
+    }
+    burg();
+    $(document).on('click', function(e){
+        if( $(e.target).closest('.burger-btn').length || $(e.target).closest('.burger-body').length)
+        return
+        $('.burger-wrap').removeClass('opened');
+        $('html').removeClass('ovf');
+    });
+});
